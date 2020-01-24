68e41f9353e0e6c86257f20e387032391b64dbc7
BetoCruz <humbertocruz.sp@gmail.com>
2020-01-24 16:04:15 -0300

    imc_calculo

---
+++ Aula1/index.js
@@ -0,0 +1,15 @@
+//ferramentas e tipos de dados 
+console.log("humberto");
+console.log('"Humberto"');
+console.log(`"Humberto" 'Cruz'`);
+console.log(35,15.85, ' Humberto Almeida da Cruz')
+console.log('eu nome é humberto eu domino o javascript as ' ,10 ,'hora da manha!')
+
+//Não podemos criar variaveis com palavras reservadas
+//Variaveis precisam ter nomes significativos 
+//Não podem começar com numeros
+//Não podem conter espaços ou traços
+//Utilizamos camelCase
+//Case-sencitive
+//não podemos Redeclarar variaveis com let
+//Não utilize var , utilize let.
