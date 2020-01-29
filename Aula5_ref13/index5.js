4bd28b70c744c89fa9bdece2c1fe4a2d9fae2af6
BetoCruz <humbertocruz.sp@gmail.com>
2020-01-29 11:24:42 -0300

    Exercicios para manipulação de dados em HTML em camadas

---
+++ Aula5_ref13/index5.js
@@ -0,0 +1,10 @@
+const nome = prompt("Digite seu nome coompleto:");
+        document.body.innerHTML += ` O  seu nome é : ${nome}<br/>`;
+        document.body.innerHTML += `Seu nome tem qtas letras: ${nome.length}<br/>`;
+        document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br/>`;
+        document.body.innerHTML +=  `Qual o primeiro indice da letra "e" do seu nome:${nome.indexOf("e")}<br/>`;  
+        document.body.innerHTML += `Qual o último indice da letra "o" do seu nome :${nome.lastIndexOf("o")}<br/>`;       
+        document.body.innerHTML += `As últimas tres letras do seu nome são:${nome.slice(-3,nome.length)}<br/>`;
+        document.body.innerHTML += `As paçavras do seu nome são :${nome.split(" ")} <br/>`;
+        document.body.innerHTML += `Seu nome com letras maiúsculas${nome.toUpperCase()} <br/>`;        
+        document.body.innerHTML += `Seu nome com letras minúsculas:${nome.toLowerCase()} <br/>`;
