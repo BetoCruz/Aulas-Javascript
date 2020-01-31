194e91e67f24a773dd1a8521228220236fe5deec
BetoCruz <humbertocruz.sp@gmail.com>
2020-01-31 11:35:18 -0300

    Template literals são literais string que permitem expressões embutidas

--- Aula5_ref13/index5.js
+++ Aula5_ref13/index5.js
@@ -1,10 +1,11 @@
 const nome = prompt("Digite seu nome coompleto:");
-        document.body.innerHTML += ` O  seu nome é : ${nome}<br/>`;
-        document.body.innerHTML += `Seu nome tem qtas letras: ${nome.length}<br/>`;
-        document.body.innerHTML += `A segunda letra do seu nome é: ${nome[1]}<br/>`;
-        document.body.innerHTML +=  `Qual o primeiro indice da letra "e" do seu nome:${nome.indexOf("e")}<br/>`;  
-        document.body.innerHTML += `Qual o último indice da letra "o" do seu nome :${nome.lastIndexOf("o")}<br/>`;       
-        document.body.innerHTML += `As últimas tres letras do seu nome são:${nome.slice(-3,nome.length)}<br/>`;
-        document.body.innerHTML += `As paçavras do seu nome são :${nome.split(" ")} <br/>`;
-        document.body.innerHTML += `Seu nome com letras maiúsculas${nome.toUpperCase()} <br/>`;        
-        document.body.innerHTML += `Seu nome com letras minúsculas:${nome.toLowerCase()} <br/>`;
+
+document.body.innerHTML += ` O  seu nome é : <strong>${nome}</strong><br/>`;
+document.body.innerHTML += `Seu nome tem qtas letras:<strong> ${nome.length} </strong><br/>`;
+document.body.innerHTML += `A segunda letra do seu nome é:<strong> ${nome[1]} </strong><br/>`;
+document.body.innerHTML +=  `Qual o primeiro indice da letra "e" do seu nome:<strong> ${nome.indexOf("e")}</strong><br/>`;  
+document.body.innerHTML += `Qual o último indice da letra "o" do seu nome :<strong> ${nome.lastIndexOf("o")}</strong><br/>`;       
+document.body.innerHTML += `As últimas tres letras do seu nome são:<strong> ${nome.slice(-3,nome.length)}</strong><br/>`;
+document.body.innerHTML += `As paçavras do seu nome são :<strong> ${nome.split(" ")}</strong> <br/>`;
+document.body.innerHTML += `Seu nome com letras maiúsculas:<strong> ${nome.toUpperCase()}</strong> <br/>`;        
+document.body.innerHTML += `Seu nome com letras minúsculas:<strong> ${nome.toLowerCase()}</strong> <br/>`;
